1c700b65b2ab8d4456cef097d40d8c3255703c65
daveatnclud <dave@nclud.com>
2010-05-04 18:13:11 -0400

    header.php > Start looking for READMEs the smartway

--- header.php
+++ header.php
@@ -5,17 +5,17 @@
     // A few configuration values.  Change these as you see fit.
     //=======================================================================
 
-    // show_readme
+    // showReadme
     //   If true, the contents of an (optional) readme.html file will appear before
     //   the directory listing.  This file should be an HTML snippet; no head/body/etc
     //   tags.  You can do paragraph tags or whatever.
-    $show_readme = true;
+    $showReadme = true;
 
-    // titleformat
+    // titleFormat
     //   How to format the <title> and <h1> text.  %DIR is replaced with the directory path.
     // for instance:
-    //   $titleformat = "Now Viewing: %DIR";
-    $titleformat = "Index of %DIR";
+    //   $titleFormat = "Now Viewing: %DIR";
+    $titleFormat = "Index of %DIR";
     
     // indices path
     $indicesPath = '/mindexes';
@@ -28,8 +28,9 @@
     $uri = preg_replace("/\?.*$/", "", $uri);
     $uri = preg_replace("/\/ *$/", "", $uri);
     
+    $absPath = $_SERVER["DOCUMENT_ROOT"] . $uri;
 
-    $titletext = str_replace("%DIR", $uri, $titleformat). '/';
+    $titletext = str_replace("%DIR", $uri, $titleFormat). '/';
 
     // generate title path, with links to each parent folder
     $folders = explode('/',$uri);
@@ -44,16 +45,42 @@
         $pathMarkup .= '<strong><a href="'.$link.'">'.$folder.'/</a></strong>';
     }    
     
-    $h1text = str_replace("%DIR", $pathMarkup, $titleformat);
+    $h1text = str_replace("%DIR", $pathMarkup, $titleFormat);
 
     // this is hacky, but in almost every situation there's no real harm.
     // it just might fail if you're doing something funky with directory mappings.
-    $readmetext = "";
-    $pathtext = "";
-    $readmefile = $_SERVER["DOCUMENT_ROOT"] . $uri . "/README.textile";
-    if ($show_readme && file_exists($readmefile)) {
-        $readmetext = "<div class='readme'>" . file_get_contents($readmefile) . "</div>";
+    // $readmetext = "";
+    // $readmefile = $absPath . "/README.textile";
+    // if ($showReadme && file_exists($readmefile)) {
+    //     $readmetext = <div class=readme> . file_get_contents($readmefile) . "</div>";
+    // }
+
+    // Textile
+    // require_once( $_SERVER["DOCUMENT_ROOT"]. $indicesPath . '/textile.php');
+    // $readmeFiles = glob('README.*');
+    // $readmeRaw = file_get_contents($readmeFiles[0]);
+    // $readmeFormatted = str_replace("\n", '<br />', $readmeRaw);
+    //         
+    // $textile = new Textile();
+
+    // Markdown
+    // require_once( $_SERVER["DOCUMENT_ROOT"]. $indicesPath . '/markdown.php');
+    // $readmeFiles = glob( $absPath .'/README.*');
+    // $readmeRaw = file_get_contents($readmeFiles[0]);
+
+    $readmeMarkup = '';
+
+    if ($showReadme) {
+        foreach (glob($absPath . '*') as $file) {
+            if( preg_match('/^README(\.[A-z0-9]+)?$/i', $file) ) {
+                $readmeFile = $file;
+                break;
+            }
+        }
+        $readmeMarkup = $readmeFile;
     }
+    
+    
 
 
 ?>
@@ -79,26 +106,28 @@
 </head>
 
 <body>
+    <?php
+    $currentDir = $absPath . '/';
+    if (is_dir($currentDir)) {
+        if ($dh = opendir($currentDir)) {
+            while (($file = readdir($dh)) !== false) {
+                // echo "filename: $file : filetype: " . filetype($dir . $file) . '<br />';
+                echo $file . '<br />';
+            }
+            closedir($dh);
+        }
+    } else {
+        echo $currentDir . ' is not a dir';
+    }
+    ?>
+    
     <div id="pagecontainer">
         
-        <?php
-            // Textile
-            // require_once( $_SERVER["DOCUMENT_ROOT"]. $indicesPath . '/textile.php');
-            // $readmeFiles = glob('README.*');
-            // $readmeRaw = file_get_contents($readmeFiles[0]);
-            // $readmeFormatted = str_replace("\n", '<br />', $readmeRaw);
-            //         
-            // $textile = new Textile();
-            
-            require_once( $_SERVER["DOCUMENT_ROOT"]. $indicesPath . '/markdown.php');
-            $readmeFiles = glob($_SERVER["DOCUMENT_ROOT"]. $uri .'/README.*');
-            $readmeRaw = file_get_contents($readmeFiles[0]);
-            
-        ?>
-
-
+        <p><?php echo $_SERVER['REQUEST_URI']; ?></p>
+        
         <div class="header">
             <h1><?= $h1text ?></h1>
-            <?php echo Markdown($readmeRaw); ?>
-
+            
+            <?= $readmeMarkup ?>
+            <p><?= 'php done'?></p>
         </div>
