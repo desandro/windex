e1a97daa9c9b0887978301ad9b5ff6851827b4b8
daveatnclud <dave@nclud.com>
2010-05-25 17:39:36 -0400

    Move all files back into /windex subfolder

--- windex/header.php
+++ windex/header.php
@@ -4,7 +4,8 @@
 <html>
 <head>
     <!--
-         Minimal directory listings generated with Windex
+         This index has been cleaned with Windex
+         http://github.com/desandro/windex
          
          A mod of Indices:
          http://antisleep.com/software/indices
@@ -16,7 +17,7 @@
     <meta name="viewport" content=" initial-scale=1.0; minimum-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
 
     <link rel="stylesheet" media="screen and (max-width: 480px)" href="<?= $windexPath ?>/css/iphone.css" />
-    <link rel="stylesheet" media="screen and (min-width: 481px)" href="<?= $windexPath ?>/css/iphone.css">
+    <link rel="stylesheet" media="screen and (min-width: 481px)" href="<?= $windexPath ?>/css/screen-foxy.css">
 
 </head>
 
