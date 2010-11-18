5b65dc2621994d9036a5a9d451608db9da662e33
dwichers <danielwichers@gmail.com>
2010-11-18 14:18:55 +0100

    :nth selectors now working in IE with use of jQuery

--- windex/header.php
+++ windex/header.php
@@ -18,6 +18,25 @@
 
     <link rel="stylesheet" media="screen and (max-width: 480px)" href="<?php echo $windexPath; ?>/css/iphone.css" />
     <link rel="stylesheet" media="screen and (min-width: 481px)" href="<?php echo $windexPath; ?>/css/screen-foxy.css" />
+    <!--[if IE]>
+        <link rel="stylesheet" media="screen" href="<?php echo $windexPath; ?>/css/screen-foxy.css" />
+    <![endif]-->    
+
+    <script src="<?php echo $windexPath; ?>/js/jquery-1.4.4.js"></script>
+    <script type="text/javascript">
+        $(function() {
+            $("tr td:nth-child(1)").css(
+                {"text-align": "center", "width": "24px", "padding": "1px 10px 1px 0"})
+            
+            $("tr td:nth-child(2), tr th:nth-child(2)").css(
+                {"width": "460px", "text-align": "left"})
+            
+            $("tr td:nth-child(3), tr td:nth-child(4)").css(
+                {"padding-left": "10px", "text-align": "right", "font-size": "11px"})
+            
+            $("tr td:nth-child(5)").css({"display": "none"});
+        });
+    </script>
 
 </head>
 
